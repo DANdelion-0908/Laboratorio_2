8e9a7d8cffbfbcada2c6d9860a0b6636eea8d0b8
Dan <98132259+Dan090807@users.noreply.github.com>
2022-09-19 17:43:33 -0600

    Ram constructor created

--- Laboratorio2/src/model/ram.java
+++ Laboratorio2/src/model/ram.java
@@ -1,5 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ram {
+	
+	public String ramType;
+	public int ramStorage;
+	public ArrayList<program> programsEXE; 
+	public ArrayList<program> programsLINE;
+	
+
+	public ram(String ramType, int ramStorage){
+		programsEXE = new ArrayList<program>();
+		programsLINE = new ArrayList<program>();
 
 }
+	
+	public String getRamType() {
+		return ramType;
+	}
+	
+	public void setRamType(String ramType) {
+		this.ramType = ramType;
+	}
+	
+	public int getRamStorage() {
+		return ramStorage;
+	}
+	
+	public void setRamStorage(int ramStorage) {
+		this.ramStorage = ramStorage;
+	}
+	
+	public ArrayList getProgramsEXE() {
+		return programsEXE;
+	}
+	
+	public void setProgramsEXE(ArrayList programsEXE) {
+		this.programsEXE = programsEXE;
+	}
+	public ArrayList getProgramsLINE() {
+		return programsLINE;
+	}
+	
+	public void setProgramsLINE(ArrayList programsLINE) {
+		this.programsLINE = programsLINE;
+	
+}
+	}
+
+
