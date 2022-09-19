2c52c8e5f6420f3d2847d50a079c38eb236c9f1c
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-09-19 17:19:55 -0600

    Added File reader functionality.

--- Laboratorio2/src/controller/fileManager.java
+++ Laboratorio2/src/controller/fileManager.java
@@ -1,5 +1,46 @@
 package controller;
+import model.ram;
+import model.program;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class fileManager {
-
+	private static final int PROGRAM_NAME = 0;
+	private static final int PROGRAM_SIZE = 1;
+	private static final int PROGRAM_TIME = 2;
+	
+	public void GetFilePrograms(ram _ram) {
+		program program;
+		String file = "src\\programasIniciales.csv";
+		BufferedReader reader = null;
+		String line;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			int count = 0;
+			while((line = br.readLine()) != null) {
+				
+				String[] fields = line.split(",");
+				
+				String name = fields[PROGRAM_NAME];
+				int size =  Integer.parseInt(fields[PROGRAM_SIZE]);
+				int time = Integer.parseInt(fields[PROGRAM_TIME]);
+				
+				program = new program(name, size, time);
+				_ram.getProgramsLINE().add(program);
+		
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
