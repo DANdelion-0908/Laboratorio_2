c42fb3d90c4b52c6da423ef83857b5741e73b6bc
Dan <98132259+Dan090807@users.noreply.github.com>
2022-09-19 17:39:14 -0600

    Creator program, getters ans setters created

--- Laboratorio2/src/model/program.java
+++ Laboratorio2/src/model/program.java
@@ -1,5 +1,38 @@
 package model;
 
 public class program {
+	
+	public String name;
+	public int programSize;
+	public int runTime;
+	
+	public program(String name, int programSize, int runTime) {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRunTime() {
+		return runTime;
+	}
+	
+	public void setRunTime(int runTime) {
+		this.runTime = runTime;
+	}
+	
+	public int getProgramSize() {
+		return programSize;
+	}
+	
+	public void setProgramSize(int programSize) {
+		this.programSize = programSize;
+	}
+
 
 }
